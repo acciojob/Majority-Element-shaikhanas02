d6350a03155e699bcae91e15bd9809d912d99614
shaikhanas02 <122043549+shaikhanas02@users.noreply.github.com>
2023-03-19 12:23:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 //your code here
+function majorityElement(nums) {
+  let count = 0;
+  let candidate = nums[0];
+  
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === candidate) {
+      count++;
+    } else {
+      count--;
+      if (count === 0) {
+        candidate = nums[i];
+      }
+    }
+  }
+  
+  // Count the occurrences of the candidate element
+  count = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === candidate) {
+      count++;
+    }
+  }
+  
+  // Check if the candidate element appears more than floor(n/2) times
+  if (count > Math.floor(nums.length / 2)) {
+    return candidate;
+  } else {
+    return -1; // Return -1 if the majority element doesn't exist
+  }
+}
