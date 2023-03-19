a99a351722486e92eabdf392b3a15edfe3827e51
shaikhanas02 <122043549+shaikhanas02@users.noreply.github.com>
2023-03-19 12:24:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function majorityElement(nums) {
   let count = 0;
   let candidate = nums[0];
   
-  for (let i = 0; i < nums.length; i++) {
+  for (let i = 1; i < nums.length; i++) {
     if (nums[i] === candidate) {
       count++;
     } else {
